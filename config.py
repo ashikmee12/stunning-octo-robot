0e77d03814aaf363324de22a0e7b23f658f5e4e2
ashikmee12 <ashik.mee12@gmail.com>
2026-04-11 16:57:47 +0600

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8792991594:AAHarRh3aJuV3mULjzNONlOrRxj_WtVNF9Y")
 OWNER_ID = int(os.environ.get("OWNER_ID", 7406197326))
-MINI_APP_URL = os.environ.get("MINI_APP_URL", "https://ha-ad2.onrender.com")
+MINI_APP_URL = os.environ.get("MINI_APP_URL", "https://miniapp-idc4.onrender.com")
 
 DB_NAME = "file_store.db"
 
